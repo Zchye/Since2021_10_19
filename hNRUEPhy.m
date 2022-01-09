50bad2a76ab850c310cc1ced04a3b480d9f7b2b5
Zchye <yuxiao.zhai@mail.utoronto.ca>
2022-01-08 19:00:00 -0500

    Extract effective SINR. Average SINR's by entropic mean. Discard DMRS SINR

--- hNRUEPhy.m
+++ hNRUEPhy.m
@@ -1017,13 +1017,13 @@ classdef hNRUEPhy < hNRPhyInterface
                 
                 %YXC begin
                 % Store SINR in YUO based on DMRS 
-                L_csi = length(csi);
-                mean_csi = zeros(L_csi,1);
-                for ii = 1:L_csi
-                    mean_csi(ii) = mean(csi{ii}(:));
-                end
-                DMRSSINR = mean(mean_csi)/noiseEst-1;   % SINR in linear scale
-                obj.YusUtilityParameter.YUO.storeDMRSSINR(DMRSSINR);
+%                 L_csi = length(csi);
+%                 mean_csi = zeros(L_csi,1);
+%                 for ii = 1:L_csi
+%                     mean_csi(ii) = mean(csi{ii}(:));
+%                 end
+%                 DMRSSINR = mean(mean_csi)/noiseEst-1;   % SINR in linear scale
+%                 obj.YusUtilityParameter.YUO.storeDMRSSINR(DMRSSINR);
                 %YXC end
                 
                 [decbits, crcFlag] = obj.DLSCHDecoder(dlschLLRs, pdschInfo.PDSCHConfig.Modulation, ...
@@ -1073,7 +1073,8 @@ classdef hNRUEPhy < hNRPhyInterface
                     rank = obj.RankIndicator;
                     %YXC begin
 %                     [cqi, pmiSet, ~, ~] = hCQISelect(carrier, csirsInfo, obj.CSIReportConfig, rank, Hest, nVar, obj.SINRTable);
-                    [cqi, pmiSet, cqiInfo, ~] = hCQISelect(carrier, csirsInfo, obj.CSIReportConfig, rank, Hest, nVar, obj.SINRTable);
+                    [cqi, pmiSet, ~, ~] = hCQISelect(carrier, csirsInfo, obj.CSIReportConfig, rank, Hest, nVar, obj.SINRTable);
+                    [~, ~, cqiInfo, ~] = yCQISelect(carrier, csirsInfo, obj.CSIReportConfig, rank, Hest, nVar, obj.SINRTable);
                     %Store cqiInfo into YusUtilityObj
                     obj.StoreCQIInfo(cqiInfo);
                     %YXC end
