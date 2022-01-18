33e0e2e48ebaba1bab28a9a5cdc26bb8cba2df8c
Yuxiao Zhai <yuxiao.zhai@mail.utoronto.ca>
2022-01-18 13:28:38 -0500

    Make up missed patches to yPRGPrecode.m and yPrecodedSINR.m

--- yPrecodedSINR.m
+++ yPrecodedSINR.m
@@ -1,5 +1,6 @@
-function sinr = yPrecodedSINR(H, nVar, P)
-    % Returns SINR of a MIMO channel for unit-power symbols
+function [sinr, F] = yPrecodedSINR(H, nVar, P)
+    % Returns sinr, SINR of a MIMO channel for unit-power symbols and
+    % F, collection of precoders for each subcarrier and symbol
     %
     % H - An array of size K-by-L-by-R-by-nLayer, where K is number of
     % subcarriers, L is the number of symbols, R is the number of receive
@@ -17,7 +18,7 @@ function sinr = yPrecodedSINR(H, nVar, P)
     % Construct function handles for calculating unnormalized precoder
     MFPrecoder = @(H) H';
     ZFPrecocer = @(H) H'/(H*H');
-    DesigPrecoder = @(H) H;
+    DesigPrecoder = @(H) P;
     
     % Parse input
     if ischar(P)||isstring(P)
@@ -38,7 +39,7 @@ function sinr = yPrecodedSINR(H, nVar, P)
             otherwise
                 error('Not implement yet')
         end
-    elseif ismatrix(P)
+    elseif isa(P, 'double')
         Pcdr = DesigPrecoder;
     else
         error('"P" must be of either char array/string or vector/matrix')
@@ -49,16 +50,25 @@ function sinr = yPrecodedSINR(H, nVar, P)
     finv = @(x) 2.^x - 1;
     effsum = @(X) finv(sum(f(X),'omitnan'));
     
-    % Calculate the SINR values as per LMMSE estimation method per
+    % Calculate the SINR values as per LMMSE estimation method and precoders per
     % subcarrier per symbol
+    F = zeros(size(H,1,2,4,3)); % Initialize collection of precoders
     [NumSubcar, NumSym] = size(H,1,2); % Get the numbers of subcarriers and symbols
     sinrGrid = zeros(NumSubcar, NumSym);
     for k = 1:NumSubcar
         for l = 1:NumSym
             Htemp = H(k,l,:,:);
             Htemp = reshape(Htemp,size(H,3,4));
-            W = Pcdr(Htemp); % Get precoder
+            W_ = Pcdr(Htemp); % Get precoder
+            if ismatrix(W_)
+                W = W_;
+            elseif ndims(W_)==4 % if P is a collection of precoders for each subcarrier and each symbol
+                W = squeeze(W_(k,l,:,:));
+            else
+                error('Invalid P.');
+            end
             W = W/norm(W,'fro'); % Normalize the precoder
+            F(k,l,:,:) = W;
             noise = nVar*eye(size(W,2)); % Noise variance multiplied by identity matrix
             den = noise/((W'*Htemp')*Htemp*W+noise);
             LayeredSinr = real(((1./diag(den))-1));
