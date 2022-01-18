477da9394f37cb4f4f731579118f604521682456
Yuxiao Zhai <yuxiao.zhai@mail.utoronto.ca>
2022-01-17 22:27:43 -0500

    yPRGPrecode.m in progress

--- yPRGPrecode.m
+++ yPRGPrecode.m
@@ -1,7 +1,12 @@
 function [antsym,antind] = yPRGPrecode(siz,nstartgrid,portsym,portind,F)
     %
     if nstartgrid
-        % Skip this input argument
+        % Ignore this input argument
     end
+    [K,L] = size(siz,1,2); % K, number of subcarriers; L, number of symbols
+    nLayer = size(F,4); % nLayer, numerb of layers
+    PortGridSize = [K,L,nLayer]; % Size of port grid
+    PortGrid = zeros(PortGridSize); % Port grid
+    PortGrid(portind(:)) = portsym(:); % Reconstruct port grid
     
 end
